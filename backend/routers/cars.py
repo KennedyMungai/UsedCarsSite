ac128ecf746c63b5fd719910d3b1627abe492176
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-17 09:23:32 +0300

    Created the list_cars endpoint

--- backend/routers/cars.py
+++ backend/routers/cars.py
@@ -3,3 +3,13 @@ from fastapi import APIRouter
 
 
 router = APIRouter()
+
+
+@router.get("/", response_description="List all cars")
+async def list_cars() -> dict:
+    """Amn api endpoint for getting the cars
+
+    Returns:
+        dict: A message to show the proper running of the code
+    """
+    return {"data": "All cars will go here"}
