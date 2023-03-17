0fdb6802486eedadde44d4a4efb11f505d1fbdc8
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-17 08:53:54 +0300

    Imported the motor db driver

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,6 @@
 """The main script for the backend"""
 from fastapi import FastAPI
 from dotenv import load_dotenv, find_dotenv
+from motor.motor_asyncio import AsyncIOMotorClient
 
 ENV = find_dotenv(load_dotenv())
