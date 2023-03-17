9ab35b7058579852fde0dc22eedbd4cf2756442c
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-17 09:38:51 +0300

    Added a docstring for the create_car endpoint

--- backend/routers/cars.py
+++ backend/routers/cars.py
@@ -19,6 +19,15 @@ async def list_cars() -> dict:
 
 @router.post("/", response_description="Add a new car")
 async def create_car(request: Request, car: CarBase = Body(...)):
+    """An endpoint for creating a new car endpoint
+
+    Args:
+        request (Request): No Idea
+        car (CarBase, optional): _description_. Defaults to Body(...).
+
+    Returns:
+        _type_: _description_
+    """
     car = jsonable_encoder(car)
     new_car = await request.app.mongodb["cars1"].find_one({"_id": new_car.inserted_id})
 
