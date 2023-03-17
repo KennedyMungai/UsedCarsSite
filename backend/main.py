7301f292ab359921085e2260c0591a3175ee2e1e
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-17 08:58:38 +0300

    Added some code to run on database close

--- backend/main.py
+++ backend/main.py
@@ -16,3 +16,8 @@ app = FastAPI()
 async def startup_db_client():
     app.mongodb_client = AsyncIOMotorClient(DB_URL)
     app.mongodb = app.mongodb_client[DB_NAME]
+    
+
+@app.on_event("shutdown")
+async def shutdown_db_client():
+    app.mongodb_client.close()
